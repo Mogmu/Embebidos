85816fdfabd4531f3bd19d19b3a9277c659ab125
Mogmu <152646292+Mogmu@users.noreply.github.com>
2023-12-02 13:39:49 -0600

    Create LCD_PORTD.h

---
+++ LCD_PORTD.h
@@ -0,0 +1,21 @@
+#ifndef LCD_PORTD_H
+#define LCD_PORTD_H
+
+#include <xc.h> // include processor files - each processor file is guarded.
+#include "LCD_Comands.h"
+#define _XTAL_FREQ 8000000
+
+#define RS PORTEbits.RE0 //McLab2
+//#define RS PORTEbits.RE2 //PicGenios
+#define EN PORTEbits.RE1
+
+void iniLCD(void);
+void LCDcommand(char a);
+void LCDchar(char a);
+void LCDprint(int size, char a[], int wait_ms);
+void MoveCursor(char x, char y);
+void MoveLCD(char dir, char inc);
+void GenChar(char a[8]);
+void LCDint(int a);
+
+#endif /* LCD_PORTD_H */
